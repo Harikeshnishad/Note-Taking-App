549c4fb72f12db47ef9cb5d772e6149391c593e6
Harikeshnishad <159423323+Harikeshnishad@users.noreply.github.com>
2024-03-20 21:29:36 +0530

    Initial commit

---
+++ app/src/main/java/com/hari/notetakingapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.hari.notetakingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
+        noteViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
+                .create(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+
+                // update RecyclerView
+                adapter.setNotes(notes);
+            }
+        });
+
+    }
+}
